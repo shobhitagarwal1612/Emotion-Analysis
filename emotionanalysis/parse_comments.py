1ab784f230876154695ef0d10e5861d5d014b6a9
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-02-13 04:36:52 +0530

    Scrapes comments from amazon's review page

--- emotionanalysis/parse_comments.py
+++ emotionanalysis/parse_comments.py
@@ -2,30 +2,19 @@ import httplib2
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 
-html_doc = open('html_doc.html')
-
-# soup = BeautifulSoup(html_doc, 'html.parser')
-# print(soup.prettify())
-# for a in soup.find_all('a'):
-#     print(a.string)
+url = 'http://www.amazon.in/TomTom-Spark-1RE0-002-01-Fitness-Watch/dp/B01BF8V7Z2/ref=sr_1_1?ie=UTF8' \
+      '&qid=1486800606&sr=8-1'
 
 ###amazon###
-h = httplib2.Http(".cache")
-resp, content = h.request('http://www.amazon.in/TomTom-Spark-1RE0-002-01-Fitness-Watch/dp/B01BF8V7Z2/ref=sr_1_1?ie=UTF8'
-                          '&qid=1486800606&sr=8-1', 'GET')
+h = httplib2.Http()
+resp, content = h.request(url, 'GET')
 
 soup = BeautifulSoup(content, 'lxml', parse_only=SoupStrainer('a'))
-# print((soup.prettify()))
 link = soup.find('a', {'class': 'a-link-emphasis a-text-bold'})
 print(link['href'] + "\n")
 
 resp, response = h.request(link['href'], 'GET')
-# print(resp)
 soup = BeautifulSoup(content, 'lxml')
 tag = soup.find('div', id='a-page') \
     .find_all('div')[1] \
     .find('div')
-
-# print(tag)
-# for span in soup.select('span.a-color-base'):
-#     print(span.text)
--- emotionanalysis/scrape_amazon.py
+++ emotionanalysis/scrape_amazon.py
@@ -6,9 +6,11 @@ import re
 from time import sleep
 
 import requests
+from bs4 import BeautifulSoup
 from lxml import html
 
 
+##home page
 def ParseReviews(url, asin):
     # This script has only been tested with Amazon.com
     amazon_url = 'http://' + url + '/dp/' + asin
@@ -109,11 +111,87 @@ def ParseReviews(url, asin):
     return data
 
 
+###review page
+def ParseReviews2(url, asin):
+    # This script has only been tested with Amazon.com
+    amazon_url = 'http://' + url + '/product-reviews/' + asin
+    # Add some recent user agent to prevent amazon from blocking the request
+    # Find some chrome user agent strings  here https://udger.com/resources/ua-list/browser-detail?browser=Chrome
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 '
+                      'Safari/537.36'}
+    page = requests.get(amazon_url, headers=headers).text
+
+    # parser = html.fromstring(page)
+    # XPATH_AGGREGATE = '//span[@id="acrCustomerReviewText"]'
+    # XPATH_REVIEW_SECTION = '//div[@id="revMHRL"]/div'
+    # XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
+    # XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
+    # XPATH_PRODUCT_PRICE = '//span[@id="priceblock_ourprice"]/text()'
+
+    soup = BeautifulSoup(page, 'lxml')
+    XPATH_PRODUCT_NAME = soup.select_one('.a-link-normal')
+    XPATH_PRODUCT_PRICE = soup.select_one('.arp-price,.a-link-normal')
+    XPATH_REVIEW_SECTION = soup.find(
+        attrs={'class': 'a-text-left a-fixed-left-grid-col reviewNumericalSummary celwidget '
+                        'a-col-left'})
+    XPATH_RATINGS_TABLE = XPATH_REVIEW_SECTION.find('table', id='histogramTable')
+    XPATH_REVIEW_LIST = soup.find('div', id='cm_cr-review_list')
+
+    product_price = XPATH_PRODUCT_PRICE.text.replace(',', '')
+    product_name = XPATH_PRODUCT_NAME.text.strip()
+    total_ratings = XPATH_REVIEW_SECTION.select_one('.totalReviewCount').text
+    reviews = XPATH_REVIEW_LIST.find_all('div', attrs={'class': 'a-section review'})
+
+    ratings_dict = {}
+    reviews_list = []
+
+    # grabing the rating  section in product page
+    for ratings in XPATH_RATINGS_TABLE:
+        extracted_rating = ratings.find_all('td')
+        if extracted_rating:
+            rating_key = extracted_rating[0].a.text
+            rating_value = extracted_rating[2].a.text
+            if rating_key:
+                ratings_dict.update({rating_key: rating_value})
+
+    # Parsing individual reviews
+    for review in reviews:
+        review = review.div
+        raw_review_author = review.find('a', class_='author').text
+        raw_review_rating = review.find('i', class_='review-rating').span.text
+        raw_review_header = review.find('a', class_='review-title').text
+        raw_review_posted_date = review.find('span', class_='review-date').text.split(' ', 1)[1]
+        raw_review_text = review.find('span', class_="a-size-base review-text").text
+
+        # cleaning data
+        review_rating = ''.join(raw_review_rating).replace('out of 5 stars', '')
+
+        review_dict = {
+            'review_text': raw_review_text,
+            'review_posted_date': raw_review_posted_date,
+            'review_header': raw_review_header,
+            'review_rating': review_rating,
+            'review_author': raw_review_author
+
+        }
+        reviews_list.append(review_dict)
+
+    data = {
+        'ratings': ratings_dict,
+        'reviews': reviews_list,
+        'url': amazon_url,
+        'price': product_price,
+        'name': product_name
+    }
+    return data
+
+
 def ReadAsin(domain, asin):
     # Add your own ASINs here
     extracted_data = []
     print("Downloading and processing page http://" + domain + "/dp/" + asin)
-    extracted_data.append(ParseReviews(domain, asin))
+    extracted_data.append(ParseReviews2(domain, asin))
     sleep(5)
     f = open('data.json', 'w')
     json.dump(extracted_data, f, indent=4)
@@ -128,8 +206,9 @@ def get_domain(url):
 
 
 if __name__ == '__main__':
-    url = "http://www.amazon.in/Tapfond-Oneplus-Protection-Degree-Quicksand/dp/B01HBAI9SG/ref=sr_1_1?ie=UTF8&qid" \
-          "=1486933819&sr=8-1&keywords=one+plus+3t+cover "
+    url = 'http://www.amazon.in/IZINC-Hooded-Sleeve-Cotton-T-Shirt/dp/B013WP1RDI/ref=pd_rhf_dp_s_cp_3?_encoding=UTF8' \
+          '&pd_rd_i=B013WP1RDI&pd_rd_r=1J8D15T1FTNBY5NRA064&pd_rd_w=yTCw1&pd_rd_wg=npkkg&psc=1&refRID' \
+          '=1J8D15T1FTNBY5NRA064 '
     domain = get_domain(url)
     asin = get_asin(url)
 
